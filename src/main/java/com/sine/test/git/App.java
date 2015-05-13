8c513abfd3b5244ed713d22e28838f39f221ae7a
qinglingzi <qinglingzi@b5m.com>
2015-05-13 09:54:09 +0800

    11212

--- src/main/java/com/sine/test/git/App.java
+++ src/main/java/com/sine/test/git/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World!" );
-=======
-      
-        System.out.println( "Hello World1!" );
->>>>>>> 3a82e6a751097f38b3a0d365c3864a9225c6f31f
     }
 }
